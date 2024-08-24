4126e8af5912690b1fbfd6596ef2c18dfb777c85
Alexander Barbie <alexanderbarbie@gmx.de>
2024-08-24 12:16:14 +0200

    added  dependency parameterized

--- PiCar-X/core/picarx/setup.py
+++ PiCar-X/core/picarx/setup.py
@@ -4,5 +4,5 @@ setup(name = 'picarx',
   #packages=['picarx','picarx.interfaces', 'picarx.drivers', 'picarx.emulators'],
   packages=find_packages('picarx'),
   package_dir={'': 'src'},
-  install_requires=['incremental<=22.10.0','watchdog<=4.0.0', 'smbus2', 'pyyaml', 'twisted<=22.10.0']
+  install_requires=['incremental<=22.10.0','watchdog<=4.0.0', 'smbus2', 'pyyaml', 'twisted<=22.10.0', 'parameterized']
 )
