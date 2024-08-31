2eb59b8383617177cadeab1a833e0dc64cb0e80c
Alexander Barbie <alexanderbarbie@gmx.de>
2024-08-31 18:24:59 +0200

    set link to picarx product page

--- PiCar-X/core/picarx/setup.py
+++ PiCar-X/core/picarx/setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(name = 'picarx',
-  #packages=['picarx','picarx.interfaces', 'picarx.drivers', 'picarx.emulators'],
   packages=find_packages('picarx'),
   package_dir={'': 'src'},
   install_requires=['incremental<=22.10.0','watchdog<=4.0.0', 'smbus2', 'pyyaml', 'twisted<=22.10.0', 'parameterized']
